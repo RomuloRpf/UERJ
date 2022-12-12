9d65bb222c51142a3e4e6b8d745850ba0af69139
Romulo Peigas <rpf.2008@yahoo.com.br>
2022-12-12 20:03:50 -0300

    Corrigido exercicio 3 da lista de MPI

--- Programacao_Paralela_e_Distribuida/MPI/MPI_ex3_lista.c
+++ Programacao_Paralela_e_Distribuida/MPI/MPI_ex3_lista.c
@@ -3,12 +3,11 @@
 #include <time.h>
 #include <mpi.h>
 
-
 int main(int argc, char *argv[])
 {
   double start, end, *vec;
   int size, trocas, numtasks, rank, count, dest, source, tag = 1;
-  MPI_Status Stat; 
+  MPI_Status Stat;
 
   MPI_Init(&argc, &argv);
   start = MPI_Wtime();
@@ -18,31 +17,38 @@ int main(int argc, char *argv[])
   size = atoi(argv[1]);
   trocas = atoi(argv[2]);
 
-  vec = (double *)malloc(sizeof(double)*size);
-  for(int i = 0; i < size; i++){
+  vec = (double *)malloc(sizeof(double) * size);
+  for (int i = 0; i < size; i++)
+  {
     vec[i] = -1.0;
   }
 
-  if(rank == 0) {
+  if (rank == 0)
+  {
     dest = 1;
     source = 1;
-    for(int i = 0; i < trocas; i++){
-      for(int j = 0; i < size; i++){
+    for (int i = 0; i < trocas; i++)
+    {
+      for (int j = 0; j < size; j++)
+      {
         vec[j] = 2.0;
       }
-      MPI_Send(&vec, 1, MPI_DOUBLE, dest, tag, MPI_COMM_WORLD);
-      MPI_Recv(&vec, 1, MPI_DOUBLE, source, tag, MPI_COMM_WORLD, &Stat);
+      MPI_Send(vec, size, MPI_DOUBLE, dest, tag, MPI_COMM_WORLD);
+      MPI_Recv(vec, size, MPI_DOUBLE, source, tag, MPI_COMM_WORLD, &Stat);
     }
   }
-  else{
+  else
+  {
     dest = 0;
     source = 0;
-    for(int i = 0; i < trocas; i++){
-      for(int j = 0; i < size; i++){
+    for (int i = 0; i < trocas; i++)
+    {
+      MPI_Recv(vec, size, MPI_DOUBLE, source, tag, MPI_COMM_WORLD, &Stat);
+      for (int j = 0; j < size; j++)
+      {
         vec[j] = 4.0;
       }
-      MPI_Send(&vec, 1, MPI_DOUBLE, dest, tag, MPI_COMM_WORLD);
-      MPI_Recv(&vec, 1, MPI_DOUBLE, source, tag, MPI_COMM_WORLD, &Stat);
+      MPI_Send(vec, size, MPI_DOUBLE, dest, tag, MPI_COMM_WORLD);
     }
   }
 
